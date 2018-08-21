2c9cd244b26fb6ac49e198726a6abcd18e73c333
Pudgy138 <matt.d.kaiser@gmail.com>
2018-08-20 19:22:39 -0700

    add brick draw and brick layout

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-<<<<<<< HEAD
 // Canvas Variables:
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 // Ball Variables:
+// Ball starting placement
 let x = canvas.width/2;
 let y = canvas.height-45;
+// Ball movement speed
 let dx = 3;
 let dy = -3;
-let ballRadius = 10;
+// Ball size
+let ballRadius = 12;
 
 // Paddle Variables:
+// Paddle size
 let paddleHeight = 10;
-let paddleWidth = 100;
+let paddleWidth = 105;
+// Paddle starting placement
 let paddleX = (canvas.width-paddleWidth)/2;
 let paddleY = canvas.height-20;
+// Paddle movement
 let rightPressed = false;
 let leftPressed = false;
 
 // Brick Variables:
-let brickRowCount = 3;
-let brickColumnCount = 5;
-let brickWidth = 75;
-let brickHeight = 20;
+// Brick production
+let brickRowCount = 5;
+let brickColumnCount = 15;
+// Brick sizing
+let brickWidth = 63.33;
+let brickHeight = 18;
+// 
 let brickPadding = 10;
-let brickOffsetTop = 30;
-let brickOffsetLeft = 30;
+let brickOffsetTop = 5;
+let brickOffsetLeft = 5;
 
 // Brick Creation Loop:
 let bricks = [];
@@ -33,6 +41,23 @@ for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (let r = 0; r < brickRowCount; r++) {
         bricks[c][r] = { x: 0, y: 0};
+    }
+}
+
+// draw the bricks
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
     }
 }
 
@@ -44,6 +69,7 @@ function init() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBricks();
     paddleBehavior();
     preStartBall();
 }
@@ -84,20 +110,7 @@ function keyUpHandler(e) {
     }
 }
 
-// draw the bricks
-function drawBricks() {
-    for ( let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
-            bricks[c][r].x = 0;
-            bricks[c][r].y = 0;
-            ctx.beginPath();
-            ctx.rect(0, 0, brickWidth, brickHeight);
-            ctx.fillStyle = "#000000";
-            ctx.fill();
-            ctx.closePath();
-        }
-    }
-}
+// bricks were here
 
 // draw the ball
 function drawBall() {
@@ -165,171 +178,3 @@ function draw() {
     paddleBehavior();   
 }
 
-=======
-// Canvas Variables:
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-
-// Ball Variables:
-let x = canvas.width/2;
-let y = canvas.height-45;
-let dx = 1;
-let dy = -1;
-let ballRadius = 10;
-
-// Paddle Variables:
-let paddleHeight = 10;
-let paddleWidth = 100;
-let paddleX = (canvas.width-paddleWidth)/2;
-let paddleY = canvas.height-20;
-let rightPressed = false;
-let leftPressed = false;
-
-// Brick Variables:
-let brickRowCount = 3;
-let brickColumnCount = 5;
-let brickWidth = 75;
-let brickHeight = 20;
-let brickPadding = 10;
-let brickOffsetTop = 30;
-let brickOffsetLeft = 30;
-
-// Brick Creation Loop:
-let bricks = [];
-for (let c = 0; c < brickColumnCount; c++) {
-    bricks[c] = [];
-    for (let r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0};
-    }
-}
-
-const preStartInterval = setInterval(init, 8);
-
-init();
-
-function init() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-    paddleBehavior();
-    preStartBall();
-}
-
-// arrow and spacebar listeners
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("keypress", spaceStart, false);
-
-// spacebar starts the game
-function spaceStart(e) {
-    if(e.keyCode == 32){
-    clearInterval(preStartInterval);
-    setInterval(draw, 8);
-    console.log("live game started");
-    }
-}
-
-// ball speed increment/decrement
-// I would like to increment and decrement the ball speed with the up and down arrows.
-
-// arrow keyDown for paddle movement
-function keyDownHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = true;
-    } else if (e.keyCode == 37) {
-        leftPressed = true;
-    } 
-}
-
-// arrow keyUp logic for paddle
-function keyUpHandler(e) {
-    if (e.keyCode == 39) {
-        rightPressed = false;
-    } else if (e.keyCode == 37) {
-        leftPressed = false;
-    }
-}
-
-// draw the bricks
-function drawBricks() {
-    for ( let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
-            bricks[c][r].x = 0;
-            bricks[c][r].y = 0;
-            ctx.beginPath();
-            ctx.rect(0, 0, brickWidth, brickHeight);
-            ctx.fillStyle = "#000000";
-            ctx.fill();
-            ctx.closePath();
-        }
-    }
-}
-
-// draw the ball
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fill();
-    ctx.closePath();
-}
-
-// draw the paddle
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
-    ctx.fill();
-    ctx.closePath();
-}
-
-// ball behavior once game starts
-function liveBallBehavior() {
-    if (y + dy < ballRadius) {
-        dy = -dy;
-    } else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX - ballRadius && x < paddleX + paddleWidth) {
-            dy = -dy;
-            ctx.fillStyle = randomColor({luminosity: 'dark'});
-        } else {
-            // alert("GAME OVER");
-            document.location.reload();
-        }
-    }
-
-    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-   
-   x += dx;
-   y += dy;
-}
-
-// paddle behavior for pre-start and live game
-function paddleBehavior() {
-        if (rightPressed && paddleX < canvas.width-paddleWidth) {
-            paddleX += 6;
-        } else if (leftPressed && paddleX > 0) {
-            paddleX -= 6;
-        }
-}
-
-// pre-start ball behavior
-function preStartBall() {
-    // This if statement allows the ball to move with the paddle before the game begins
-    if (rightPressed && ballRadius < paddleWidth) {
-        x += 6;
-    } else if (leftPressed && ballRadius < paddleWidth) {
-        x -= 6;
-    }
-}
-
-// 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();   
-    liveBallBehavior();     
-    paddleBehavior();   
-}
-
->>>>>>> ee488e26443e4736072caa839fab819754b2a9ed
